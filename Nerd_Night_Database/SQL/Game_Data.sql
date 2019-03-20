5f06e8ff981481b336e961a208912d8c0f141706
Trippik <43576788+Trippik@users.noreply.github.com>
2019-03-20 13:59:48 +0000

    Create Game_Data.sql

---
+++ Nerd_Night_Database/SQL/Game_Data.sql
@@ -0,0 +1,450 @@
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(1,
+"Worms",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(2,
+"Quake",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(3,
+"Sega Rally Championship",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(4,
+"Clockwork Knight",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(5,
+"Independence Day",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(6,
+"Tunnel B1",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(7,
+"Manks Superbike TT",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(8,
+"Doom",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(9,
+"Alien Trilogy",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(10,
+"Tomb Raider",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(11,
+"Daytona USA",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(12,
+"Spot Goes to Hollywood",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(13,
+"Street Fighter Alpha 2",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(14,
+"Loaded",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(15,
+"Nights",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(16,
+"Virtua Fighter 2",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(17,
+"Crime Wave",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(18,
+"Actua Golf",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(19,
+"International Victory Goal",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(20,
+"FireStorm Thunder Hawk 2",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(21,
+"Casper",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(22,
+"Theme Park",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(23,
+"Crazy Ivan",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(24,
+"NHL Hockey",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(25,
+"Sim City 2000",
+6,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(26,
+"Resident Evil",
+2,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(27,
+"Mario Kart Double Dash",
+2,
+2);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(28,
+"Street Volume 2",
+2,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(29,
+"Legends of Wrestling",
+2,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(30,
+"Dance UK",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(31,
+"Cool Boarders 3",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(32,
+"Harry Potter and the Philosophers Stone",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(33,
+"Demo Disk",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(34,
+"Frogger",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(35,
+"Croc the Legend Lives On",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(36,
+"A bugs life",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(37,
+"Colin McRays Rally 2.0",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(38,
+"LMA Manager 2001",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(39,
+"Tomb Raider - Staring Lara Croft",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(40,
+"Ridge Racer",
+3,
+1);
+
+INSERT INTO `nerd_night`.`game`
+(`id`,
+`game_name`,
+`compatible_console`,
+`quantity_total`)
+VALUES
+(41,
+"Die Hard Trilogy",
+3,
+1);
