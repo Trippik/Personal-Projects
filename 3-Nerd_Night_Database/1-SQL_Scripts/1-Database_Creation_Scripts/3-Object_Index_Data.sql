f42945a08d636404d30291826092459aecca8a51
Trippik <43576788+Trippik@users.noreply.github.com>
2019-03-21 00:10:59 +0000

    Create 3-Object_Index_Data.sql

---
+++ 3-Nerd_Night_Database/1-SQL_Scripts/1-Database_Creation_Scripts/3-Object_Index_Data.sql
@@ -0,0 +1,55 @@
+INSERT INTO `nerd_night`.`object_index`
+(`id`,
+`object_name`)
+VALUES
+(1,
+'Monitor');
+
+INSERT INTO `nerd_night`.`object_index`
+(`id`,
+`object_name`)
+VALUES
+(2,
+'TV');
+
+INSERT INTO `nerd_night`.`object_index`
+(`id`,
+`object_name`)
+VALUES
+(3,
+'General Controller');
+
+INSERT INTO `nerd_night`.`object_index`
+(`id`,
+`object_name`)
+VALUES
+(4,
+'Specialised Controller');
+
+INSERT INTO `nerd_night`.`object_index`
+(`id`,
+`object_name`)
+VALUES
+(5,
+'Memory Card');
+
+INSERT INTO `nerd_night`.`object_index`
+(`id`,
+`object_name`)
+VALUES
+(6,
+'Cable');
+
+INSERT INTO `nerd_night`.`object_index`
+(`id`,
+`object_name`)
+VALUES
+(7,
+'Adapter');
+
+INSERT INTO `nerd_night`.`object_index`
+(`id`,
+`object_name`)
+VALUES
+(8,
+'Battery');
