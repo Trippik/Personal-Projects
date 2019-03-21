a6099c77fb89b9dc736b25169bfe68f51203ecee
Trippik <43576788+Trippik@users.noreply.github.com>
2019-03-21 00:08:45 +0000

    Create 2-Console_Data.sql

---
+++ 3-Nerd_Night_Database/1-SQL_Scripts/1-Database_Creation_Scripts/2-Console_Data.sql
@@ -0,0 +1,87 @@
+INSERT INTO `nerd_night`.`console`
+(`id`,
+`console_name`,
+`quantity_total`
+)
+VALUES
+(1,
+'Raspberry Pi',
+1
+);
+
+INSERT INTO `nerd_night`.`console`
+(`id`,
+`console_name`,
+`quantity_total`
+)
+VALUES
+(2,
+'Game Cube',
+1
+);
+
+INSERT INTO `nerd_night`.`console`
+(`id`,
+`console_name`,
+`quantity_total`
+)
+VALUES
+(3,
+'Playstation 1',
+1
+);
+
+INSERT INTO `nerd_night`.`console`
+(`id`,
+`console_name`,
+`quantity_total`
+)
+VALUES
+(4,
+'Playstation 2',
+2
+);
+
+INSERT INTO `nerd_night`.`console`
+(`id`,
+`console_name`,
+`quantity_total`
+)
+VALUES
+(5,
+'Playstation 3',
+1
+);
+
+INSERT INTO `nerd_night`.`console`
+(`id`,
+`console_name`,
+`quantity_total`
+)
+VALUES
+(6,
+'Sega Saturn',
+1
+);
+
+INSERT INTO `nerd_night`.`console`
+(`id`,
+`console_name`,
+`quantity_total`
+)
+VALUES
+(7,
+'Xbox',
+1
+);
+
+INSERT INTO `nerd_night`.`console`
+(`id`,
+`console_name`,
+`quantity_total`
+)
+VALUES
+(8,
+'Xbox 360',
+2
+);
