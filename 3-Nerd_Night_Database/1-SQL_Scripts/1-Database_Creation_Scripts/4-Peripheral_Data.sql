f849deedfd03faad6f97bd2c032305a08a3a783b
Trippik <43576788+Trippik@users.noreply.github.com>
2019-03-21 00:11:58 +0000

    Create 4-Peripheral_Data.sql

---
+++ 3-Nerd_Night_Database/1-SQL_Scripts/1-Database_Creation_Scripts/4-Peripheral_Data.sql
@@ -0,0 +1,350 @@
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(1,
+'GameCube Controller',
+2,
+2,
+3);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(2,
+'Playstation 1 Controller',
+2,
+3,
+3);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(3,
+'DualShock 2 Controller',
+5,
+4,
+3);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(4,
+'Buzz Controller',
+8,
+4,
+4);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(5,
+'Sega Saturn Controller',
+2,
+6,
+3);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(6,
+'Xbox Controller',
+0,
+7,
+3);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(7,
+'Wireless Xbox 360 Controller',
+2,
+8,
+3);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(8,
+'Guitar Hero Guitar',
+1,
+8,
+4);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(9,
+'Guitar Hero Drum Kit',
+1,
+8,
+4);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(10,
+'Wired Xbox 360 Controller',
+2,
+8,
+3);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(11,
+'Guitar Hero Drumsticks',
+1,
+8,
+4);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(12,
+'Guitar Hero Microphone',
+1,
+8,
+4);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(13,
+'Monitor',
+1,
+99,
+1);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(14,
+'Sing Star Microphone',
+3,
+4,
+4);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(15,
+'iToy Camera',
+2,
+4,
+4);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(16,
+'Playstation 2 Memory Card',
+2,
+4,
+5);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(17,
+'PS3 to Composite Cable',
+1,
+5,
+6);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(18,
+'Sega Saturn Memory Card',
+1,
+6,
+5);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(19,
+'Xbox DVD Remote',
+1,
+8,
+4);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(20,
+'Xbox 360 WiFi Adapter',
+1,
+8,
+7);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(21,
+'Xbox 360 Controller Attachement',
+1,
+8,
+4);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(22,
+'AUX Cable',
+1,
+99,
+6);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(23,
+'RCA to AUX Cable',
+1,
+99,
+6);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(24,
+'USB to Mini USB Cable',
+1,
+99,
+6);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(25,
+'VGA Cable',
+1,
+99,
+6);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(26,
+'Xbox Audio Adapter',
+1,
+8,
+6);
+
+INSERT INTO `nerd_night`.`peripheral`
+(`id`,
+`peripheral_name`,
+`quantity_total`,
+`compatible_console`,
+`object`)
+VALUES
+(27,
+'Xbox 360 to VGA Cable',
+1,
+8,
+6);
