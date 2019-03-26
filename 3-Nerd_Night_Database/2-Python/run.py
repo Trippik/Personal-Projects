686c9b18944b3a19910ae787de2437d2279ed14b
Trippik <43576788+Trippik@users.noreply.github.com>
2019-03-26 20:21:03 +0000

    Update run.py

--- 3-Nerd_Night_Database/2-Python/run.py
+++ 3-Nerd_Night_Database/2-Python/run.py
@@ -1,27 +1,34 @@
 from Base_Classes import *
 
 def startup():
+#Function that writes a greeting message to the screen
     print("*************************************************")
     print("Welcome to the Nerd Night Database")
     
 
 def new_section():
+#Function that writes appropriate messages and spacing for mew menus and other new function/messages
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
     print("*************************************************")
 
 def add_game():
+#Function for adding new games to the nerd night database
     print("Please enter game title")
     game_title = input()
     print("Please enter compatible console")
     game_console = input()
     game = Game(game_title, game_console)
+    #Create object based on the game class imported from the Base_Classes file
     game.add_game()
+    #Run the add game routine for the newly created object
 
 def query_function(game_title, console, item_code):
+#Function for creating necessary object within correct class when querying the nerd night database
     print("Starting search")
     print("--------------------------------------------------------------")
     print("--------------------------------------------------------------")
     search = query(game_title, console, item_code)
+    #Create an object called search based on the query class imported from the Base_Classes file
     print("Would you like to:")
     print("1. Search for another item")
     print("2. Return to main menu")
@@ -32,10 +39,12 @@ def query_function(game_title, console, item_code):
     elif (user_response == 2):
         new_section()
         main_menu()
+    #Once the appropriate routines from the search/query object is complete return to the above sub menu
         
         
     
 def add_item():
+#Function for adding new items to the nerd night database
     print()
     print("Please select the catagory of the item you are adding")
     print("1. Game")
@@ -55,8 +64,10 @@ def add_item():
     else:
         print("Invalid response, returning to main menu")
         main_menu()
+    #Above is basic menu system
 
 def query_item():
+#Function for taking in necessary user data when querying nerd night database and passing it the query_function()
     print()
     print("Please select the catagory of the item you are looking for")
     print("1. Game")
@@ -65,6 +76,7 @@ def query_item():
     print("4. Peripheral")
     query_option = int(input())
     new_section()
+    #If user is searching for game run code below
     if (query_option == 1):
         print("Would you like to search by:")
         print("1. Game title")
@@ -79,12 +91,13 @@ def query_item():
             print("Please input console")
             console_name = input()
             query_function("IGNORE", console_name, 2)
-        elif (user_response == 3):
+        elif (user_response1 == 3):
             print("Please input game title")
             game_title = input()
             print("Please input console")
             console_name = input()
             query_function(game_title, console_name, 3)
+    #If user is searching for a console run code below        
     elif (query_option == 2):
         print("Would you like to:")
         print("1. See a list of consoles in inventory")
@@ -97,10 +110,12 @@ def query_item():
             console_name = input()
             query_function("IGNORE", console_name, 5)
         print("insert controller query code")
+    #If user is searching for a controller run code below    
     elif (query_option == 3):
         print("insert Peripheral Query Code")
 
 def option(user_option):
+#Function for processing user responses from main menu
     if (user_option == 1):
         query_item()
     elif (user_option == 2):
@@ -110,6 +125,7 @@ def option(user_option):
         
 
 def main_menu():
+#Main menu function
     print()
     print("Would you like to?")
     print("1. Query Nerd Night Inventory?")
@@ -120,8 +136,6 @@ def main_menu():
     new_section()
     option(user_input)
     
-
+#Run startup and main menu
 startup()
 main_menu()
-    
-
