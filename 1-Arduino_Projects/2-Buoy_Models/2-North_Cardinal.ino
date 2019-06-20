f59eeebe618ef89aeaa585acb15916af82cdfc98
Trippik <43576788+Trippik@users.noreply.github.com>
2019-06-20 20:29:02 +0100

    Update 2-North_Cardinal.ino

--- 1-Arduino_Projects/2-Buoy_Models/2-North_Cardinal.ino
+++ 1-Arduino_Projects/2-Buoy_Models/2-North_Cardinal.ino
@@ -1,20 +1,16 @@
 //Pin Assignment
-int led = 3;
-int button = 2;
+int led = 13;
 int flash = 200;
 int wait = 500;
 
 void setup() {
   //Pin initialisation
   pinMode(led, OUTPUT);
-  pinMode(button, INPUT);
 }
 
 void loop() {
-  if(button == 1){
-    analogWrite(led, 255);
-    delay(flash);
-    analogWrite(led, 0);
-    delay(wait);
-  }
+  analogWrite(led, 255);
+  delay(flash);
+  analogWrite(led, 0);
+  delay(wait);
 }
