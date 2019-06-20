8d8537b972f6693aedfcdb8ba67b52dc94ffdad1
Trippik <43576788+Trippik@users.noreply.github.com>
2019-06-20 13:27:12 +0100

    Create 6-Safe_Water_Mark.ino
    LED Wired to Pin 3, switch wired to Pin 2, When switch is on LED will turn on for 3 Seconds once with a 10 second delay between each flash

---
+++ 1-Arduino_Projects/2-Buoy_Models/6-Safe_Water_Mark.ino
@@ -0,0 +1,20 @@
+//Pin Assignment
+int led = 3;
+int button = 2;
+int flash = 3000;
+int wait = 10000;
+
+void setup() {
+  //Pin initialisation
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  if(button == 1){
+    analogWrite(led, 255);
+    delay(flash);
+    analogWrite(led, 0);
+    delay(wait);
+  }
+}
