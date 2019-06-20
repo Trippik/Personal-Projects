c6dce84a5ecc5f256ee68cff10c041389a9a7acb
Trippik <43576788+Trippik@users.noreply.github.com>
2019-06-20 20:34:34 +0100

    Update 6-Safe_Water_Mark.ino

--- 1-Arduino_Projects/2-Buoy_Models/6-Safe_Water_Mark.ino
+++ 1-Arduino_Projects/2-Buoy_Models/6-Safe_Water_Mark.ino
@@ -1,20 +1,16 @@
 //Pin Assignment
-int led = 3;
-int button = 2;
+int led = 13;
 int flash = 3000;
 int wait = 10000;
 
 void setup() {
   //Pin initialisation
   pinMode(led, OUTPUT);
-  pinMode(button, INPUT);
 }
 
 void loop() {
-  if(button == 1){
     analogWrite(led, 255);
     delay(flash);
     analogWrite(led, 0);
     delay(wait);
-  }
 }
