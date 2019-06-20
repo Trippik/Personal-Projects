c17cee3638a7ac66bf7e4d1c9d70e109a79c03be
Trippik <43576788+Trippik@users.noreply.github.com>
2019-06-20 20:35:35 +0100

    Update 7-Isolated_Danger_Mark.ino

--- 1-Arduino_Projects/2-Buoy_Models/7-Isolated_Danger_Mark.ino
+++ 1-Arduino_Projects/2-Buoy_Models/7-Isolated_Danger_Mark.ino
@@ -1,17 +1,14 @@
 //Pin Assignment
-int led = 3;
-int button = 2;
+int led = 13;
 int flash = 200;
 int wait = 3000;
 
 void setup() {
   //Pin initialisation
   pinMode(led, OUTPUT);
-  pinMode(button, INPUT);
 }
 
 void loop() {
-  if(button == 1){
     analogWrite(led, 255);
     delay(flash);
     analogWrite(led, 0);
@@ -20,5 +17,4 @@ void loop() {
     delay(flash);
     analogWrite(led, 0);
     delay(wait);
-  }
 }
