17dac51dcfc19b0adc78e893935092118b2392fb
Trippik <43576788+Trippik@users.noreply.github.com>
2019-06-20 13:13:45 +0100

    Create 3-East_Cardinal.ino
    LED Wired to Pin 3, switch wired to Pin 2, When switch is on LED will turn on for 0.2 Seconds three times, with a 0.2 second delay between each flash, after three flashes there will be a 3 second delay

---
+++ 1-Arduino_Projects/2-Buoy_Models/3-East_Cardinal.ino
@@ -0,0 +1,28 @@
+//Pin Assignment
+int led = 3;
+int button = 2;
+int flash = 200;
+int wait = 3000;
+
+void setup() {
+  //Pin initialisation
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT);
+}
+
+void loop() {
+  if(button == 1){
+    analogWrite(led, 255);
+    delay(flash);
+    analogWrite(led, 0);
+    delay(flash);
+    analogWrite(led, 255);
+    delay(flash);
+    analogWrite(led, 0);
+    delay(flash);
+    analogWrite(led, 255);
+    delay(flash);
+    analogWrite(led, 0);
+    delay(wait);
+  }
+}
